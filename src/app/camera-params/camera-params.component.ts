409917ebf306f3f02fd116dd0b8f6e85874c0f6c
minaru akahori <minoruaka@gmail.com>
2021-01-08 22:23:55 +0900

    modify

--- src/app/camera-params/camera-params.component.ts
+++ src/app/camera-params/camera-params.component.ts
@@ -13,7 +13,7 @@ import { ParamsSetService } from '../params-set.service';
 export class CameraParamsComponent implements OnInit {
 
   //サービスから初期値取得
-  public camera1 = this.paramsService.camera1;
+  private camera1 = this.paramsService.camera1;
 
   //パラメーター初期値
   firstParams:LUCID= {
@@ -30,10 +30,32 @@ export class CameraParamsComponent implements OnInit {
   };
 
   //設定値変数
-  lucid1: LUCID;
+  lucid1: LUCID = {
+    ConfidenceThresholdEnable: this.firstParams.ConfidenceThresholdEnable,
+    ConfidenceThresholdMin: this.firstParams.ConfidenceThresholdMin,
+    ROIParam: {
+      ROI_MinX: this.firstParams.ROIParam.ROI_MinX,
+      ROI_MinY: this.firstParams.ROIParam.ROI_MinY,
+      ROI_MinZ: this.firstParams.ROIParam.ROI_MinZ,
+      ROI_MaxX: this.firstParams.ROIParam.ROI_MaxX,
+      ROI_MaxY: this.firstParams.ROIParam.ROI_MaxY,
+      ROI_MaxZ: this.firstParams.ROIParam.ROI_MaxZ,
+    }
+  };
 
   //フォームの変数
-  paramsNum: LUCID;
+  paramsNum: LUCID = {
+    ConfidenceThresholdEnable: this.firstParams.ConfidenceThresholdEnable,
+    ConfidenceThresholdMin: this.firstParams.ConfidenceThresholdMin,
+    ROIParam: {
+      ROI_MinX: this.firstParams.ROIParam.ROI_MinX,
+      ROI_MinY: this.firstParams.ROIParam.ROI_MinY,
+      ROI_MinZ: this.firstParams.ROIParam.ROI_MinZ,
+      ROI_MaxX: this.firstParams.ROIParam.ROI_MaxX,
+      ROI_MaxY: this.firstParams.ROIParam.ROI_MaxY,
+      ROI_MaxZ: this.firstParams.ROIParam.ROI_MaxZ,
+    }
+  };
 
   //フォームグループ設定
   formParams = this.fb.group({
@@ -56,10 +78,7 @@ export class CameraParamsComponent implements OnInit {
   ) { }
 
 
-  ngOnInit(): void {
-    this.lucid1 = this.firstParams;
-    this.paramsNum = this.firstParams;
-  }
+  ngOnInit(): void { }
 
   
   /*
@@ -73,7 +92,7 @@ export class CameraParamsComponent implements OnInit {
     console.log(params);
     this.eventService.sendCameraParams(params).subscribe(() => {
       window.alert("カメラのパラメータを送信しました");
-      this.modifyObject(formParams); //送信時にオブジェクトの値を更新
+      this.modify(this.lucid1, formParams); //送信時に設定値を更新
     });
   }
 
@@ -84,21 +103,21 @@ export class CameraParamsComponent implements OnInit {
       console.log(paramData);
       if (data) {
         window.alert("パラメータを取得しました");
-        this.modifyFormParams(paramData); //取得時にフォームの値を更新
-        this.modifyObject(paramData); //送信時にオブジェクトの値を更新
+        this.modify(this.paramsNum, paramData); //取得時にフォームの値を更新
+        this.modify(this.lucid1, paramData); //送信時に設定値を更新
       }
     });
   };
 
   //初期値に戻す
   returnFirstParams() {
-    this.setFirstObject();
-    this.setFirstFormParams();
+    this.modify(this.paramsNum, this.firstParams);
+    this.modify(this.lucid1, this.firstParams);
   }
 
   //現在の設定値に戻す
   returnParams() {
-    this.modifyFormParams(this.lucid1);
+    this.modify(this.paramsNum, this.lucid1);
   }
 
 
@@ -106,21 +125,22 @@ export class CameraParamsComponent implements OnInit {
   各モジュール
   */
   
-  //オブジェクト初期値
-  setFirstObject() {
-    this.lucid1 = this.firstParams;
-  }
-  //フォーム初期値
-  setFirstFormParams() {    
-    this.paramsNum = this.firstParams;
-  }
-  //オブジェクトのパラメータ更新
-  modifyObject(parameter:LUCID) {
-    this.lucid1 = parameter;
-  } 
-  //フォームのパラメータ更新
-  modifyFormParams(parameter:LUCID) {
-    this.paramsNum = parameter;
+  /*
+  ngModelの関係で初期値に戻すときなどで
+  ＞    this.lucid1（変数） = this.firstParams（変数）;
+  と変数単位で格納するとフロント側で挙動がおかしくなる
+  要素別に値を代入すればエラー回避できる
+  */
+
+  modify(oldObj:LUCID, newObj:LUCID) {
+    oldObj.ConfidenceThresholdEnable = newObj.ConfidenceThresholdEnable;
+    oldObj.ConfidenceThresholdMin = newObj.ConfidenceThresholdMin;
+    oldObj.ROIParam.ROI_MinX = newObj.ROIParam.ROI_MinX;
+    oldObj.ROIParam.ROI_MinY = newObj.ROIParam.ROI_MinY;
+    oldObj.ROIParam.ROI_MinZ = newObj.ROIParam.ROI_MinZ;
+    oldObj.ROIParam.ROI_MaxX = newObj.ROIParam.ROI_MaxX;
+    oldObj.ROIParam.ROI_MaxY = newObj.ROIParam.ROI_MaxY;
+    oldObj.ROIParam.ROI_MaxZ = newObj.ROIParam.ROI_MaxZ;
   }
 
 }
--- src/app/recog-params/recog-params.component.ts
+++ src/app/recog-params/recog-params.component.ts
@@ -18,7 +18,7 @@ export class RecogParamsComponent implements OnInit {
   public info = this.paramsService.recogParamsInfo;
 
   //パラメータ初期値
-  firstParams:RECG_PARAMS = {
+  firstRecogParams:RECG_PARAMS = {
     BoxParam: {
       Boxthickness: this.recogParams.BoxParam[0].value,
       BoxOutWidth: this.recogParams.BoxParam[1].value,
@@ -49,10 +49,66 @@ export class RecogParamsComponent implements OnInit {
   };
 
   //設定値変数
-  recog: RECG_PARAMS;
+  nowRecogParams: RECG_PARAMS = {
+    BoxParam: {
+      Boxthickness: this.recogParams.BoxParam[0].value,
+      BoxOutWidth: this.recogParams.BoxParam[1].value,
+      BoxOutHeight: this.recogParams.BoxParam[2].value,
+      ModelStep: this.recogParams.BoxParam[3].value,
+      ModelBoxDepth: this.recogParams.BoxParam[4].value,
+      zDirection: this.recogParams.BoxParam[5].value,
+    },
+    SceneLeafSize: this.recogParams.SceneLeafSize.value,
+    PlaneNormalAngle: this.recogParams.PlaneNormalAngle.value,
+    PlaneDistanceThreshold: this.recogParams.PlaneDistanceThreshold.value,
+    EdgePointDelete_Step: this.recogParams.EdgePointDelete_Step.value,
+    LineDistanceThreshold: this.recogParams.LineDistanceThreshold.value,
+    BoxEdgeFormedAngleThreshold: this.recogParams.BoxLengthErrorThreshold.value,
+    BoxLengthErrorThreshold: this.recogParams.BoxLengthErrorThreshold.value,
+    BoxPointRadius: this.recogParams.BoxPointRadius.value,
+    BoxFramePointRate: this.recogParams.BoxFramePointRate.value,
+    MaxBoxRecogNum: this.recogParams.MaxBoxRecogNum.value,
+    VoxelPointNumThreshold: this.recogParams.VoxelPointNumThreshold.value,
+    VoxelXSize: this.recogParams.VoxelXSize.value,
+    VoxelYSize: this.recogParams.VoxelYSize.value,
+    VoxelZSize: this.recogParams.VoxelZSize.value,
+    ICPParam: {
+      icp_maxIterations: this.recogParams.ICPParam[0].value,
+      icp_euclideanFitnessEpsilon: this.recogParams.ICPParam[1].value,
+      icp_thresScore: this.recogParams.ICPParam[2].value
+    }
+  } 
 
   //フォームの変数
-  paramsNum: RECG_PARAMS;
+  recogFormParams: RECG_PARAMS = {
+    BoxParam: {
+      Boxthickness: this.recogParams.BoxParam[0].value,
+      BoxOutWidth: this.recogParams.BoxParam[1].value,
+      BoxOutHeight: this.recogParams.BoxParam[2].value,
+      ModelStep: this.recogParams.BoxParam[3].value,
+      ModelBoxDepth: this.recogParams.BoxParam[4].value,
+      zDirection: this.recogParams.BoxParam[5].value,
+    },
+    SceneLeafSize: this.recogParams.SceneLeafSize.value,
+    PlaneNormalAngle: this.recogParams.PlaneNormalAngle.value,
+    PlaneDistanceThreshold: this.recogParams.PlaneDistanceThreshold.value,
+    EdgePointDelete_Step: this.recogParams.EdgePointDelete_Step.value,
+    LineDistanceThreshold: this.recogParams.LineDistanceThreshold.value,
+    BoxEdgeFormedAngleThreshold: this.recogParams.BoxLengthErrorThreshold.value,
+    BoxLengthErrorThreshold: this.recogParams.BoxLengthErrorThreshold.value,
+    BoxPointRadius: this.recogParams.BoxPointRadius.value,
+    BoxFramePointRate: this.recogParams.BoxFramePointRate.value,
+    MaxBoxRecogNum: this.recogParams.MaxBoxRecogNum.value,
+    VoxelPointNumThreshold: this.recogParams.VoxelPointNumThreshold.value,
+    VoxelXSize: this.recogParams.VoxelXSize.value,
+    VoxelYSize: this.recogParams.VoxelYSize.value,
+    VoxelZSize: this.recogParams.VoxelZSize.value,
+    ICPParam: {
+      icp_maxIterations: this.recogParams.ICPParam[0].value,
+      icp_euclideanFitnessEpsilon: this.recogParams.ICPParam[1].value,
+      icp_thresScore: this.recogParams.ICPParam[2].value
+    }
+  };
 
   //フォームグループ設定
   BoxRecogParam = this.fb.group({
@@ -91,10 +147,7 @@ export class RecogParamsComponent implements OnInit {
     private paramsService :ParamsSetService,
   ) { }
 
-  ngOnInit(): void {
-    this.recog = this.firstParams;
-    this.paramsNum = this.firstParams;
-  };
+  ngOnInit(): void {};
 
   
   /*
@@ -108,7 +161,7 @@ export class RecogParamsComponent implements OnInit {
     console.log(params);
     this.eventService.sendRecogParams(params).subscribe(() => {
       window.alert("認識用パラメータを送信しました");
-      this.modifyObject(formParams); //送信時にオブジェクトの値を更新
+      this.modifyRecogParams(this.nowRecogParams, formParams); //送信時に設定値を更新
     });
   };
 
@@ -118,21 +171,23 @@ export class RecogParamsComponent implements OnInit {
       // const paramData:RECG_PARAMS = JSON.parse(data);
       const paramData = JSON.parse(data)
       console.log(paramData);
-      if (data) {
-        window.alert("パラメータを取得しました")
-      }
+      // if (data) {
+      //   window.alert("パラメータを取得しました");
+      //   this.modifyRecogParams(this.nowRecogParams, paramData); //設定値を更新
+      //   this.modifyRecogParams(this.recogFormParams, paramData); //フォームの値を更新
+      // }
     });
   };
 
   //初期値に戻す
   returnFirstParams() {
-    this.setFirstObject();
-    this.setFirstFormParams();
+    this.modifyRecogParams(this.nowRecogParams, this.firstRecogParams);
+    this.modifyRecogParams(this.recogFormParams, this.firstRecogParams);
   }
 
   //現在の設定値に戻す
   returnParams() {
-    this.modifyFormParams(this.recog);
+    this.modifyRecogParams(this.recogFormParams, this.nowRecogParams);
   }
 
 
@@ -141,21 +196,37 @@ export class RecogParamsComponent implements OnInit {
   各モジュール
   */
   
-  //オブジェクト初期値
-  setFirstObject() {
-    this.recog = this.firstParams;
-  }
-  //フォーム初期値
-  setFirstFormParams() {    
-    this.paramsNum = this.firstParams;
-  }
-  //オブジェクトのパラメータ更新
-  modifyObject(parameter:RECG_PARAMS) {
-    this.recog = parameter;
-  } 
-  //フォームのパラメータ更新
-  modifyFormParams(parameter:RECG_PARAMS) {
-    this.paramsNum = parameter;
-  }
+  /*
+  ngModelの関係で初期値に戻すときなどで
+  ＞    this.lucid1（変数） = this.firstParams（変数）;
+  と変数単位で格納するとフロント側で挙動がおかしくなる
+  要素別に値を代入すればエラー回避できる
+  */
 
+  modifyRecogParams(oldObj:RECG_PARAMS, newObj:RECG_PARAMS) {
+    oldObj.BoxParam.Boxthickness = newObj.BoxParam.Boxthickness;
+    oldObj.BoxParam.BoxOutWidth = newObj.BoxParam.BoxOutWidth;
+    oldObj.BoxParam.BoxOutHeight = newObj.BoxParam.BoxOutHeight;
+    oldObj.BoxParam.ModelStep = newObj.BoxParam.ModelStep;
+    oldObj.BoxParam.ModelBoxDepth = newObj.BoxParam.ModelBoxDepth;
+    oldObj.BoxParam.zDirection = newObj.BoxParam.zDirection;
+    oldObj.SceneLeafSize = newObj.SceneLeafSize;    
+    oldObj.PlaneNormalAngle = newObj.PlaneNormalAngle;
+    oldObj.PlaneDistanceThreshold = newObj.PlaneDistanceThreshold;
+    oldObj.EdgePointDelete_Step = newObj.EdgePointDelete_Step;
+    oldObj.LineDistanceThreshold = newObj.LineDistanceThreshold;
+    oldObj.BoxEdgeFormedAngleThreshold = newObj.BoxEdgeFormedAngleThreshold;
+    oldObj.BoxLengthErrorThreshold = newObj.BoxLengthErrorThreshold;
+    oldObj.BoxPointRadius = newObj.BoxPointRadius;
+    oldObj.BoxFramePointRate = newObj.BoxFramePointRate;
+    oldObj.MaxBoxRecogNum = newObj.MaxBoxRecogNum;
+    oldObj.VoxelPointNumThreshold = newObj.VoxelPointNumThreshold;
+    oldObj.VoxelXSize = newObj.VoxelXSize;
+    oldObj.VoxelYSize = newObj.VoxelYSize;
+    oldObj.VoxelZSize = newObj.VoxelZSize;
+    oldObj.ICPParam.icp_maxIterations = newObj.ICPParam.icp_maxIterations;
+    oldObj.ICPParam.icp_euclideanFitnessEpsilon = newObj.ICPParam.icp_euclideanFitnessEpsilon;
+    oldObj.ICPParam.icp_thresScore = newObj.ICPParam.icp_thresScore;
+  } 
 }
+
